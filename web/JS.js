68ff2f4775cb11572bdcff4d482f1b3e14568b7d
Nhi-228 <binhnhi5128@gmail.com>
2025-07-02 07:14:36 +0700

    update

--- web/JS.js
+++ web/JS.js
@@ -4,3 +4,22 @@
  */
 
 
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+
+function updateSlide() {
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function changeSlide(direction) {
+  currentIndex += direction;
+  if (currentIndex < 0) currentIndex = totalSlides - 1;
+  if (currentIndex >= totalSlides) currentIndex = 0;
+  updateSlide();
+}
+
+// Tự động chuyển ảnh mỗi 5 giây
+setInterval(() => {
+  changeSlide(1);
+}, 5000);
